7ee219b3eabb5f85ed47d7d434fb96fd7d5aa6b7
nhardiansa <nhardiansa@gmail.com>
2021-02-20 22:47:57 +0800

    mebuat komponen skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Header from "./Components/Header/Header"
+import Skills from "./Components/Skills/Skills"
 
 class App extends Component{
   render(){
     return (
       <div className="container">
         <Header />
+        <br/>
+        <Skills />
       </div>
     )
   }
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Skills extends Component{
+    render(){
+        return(
+            <div className="Skills">
+                <h2>Front End</h2>
+                <ul className="title">
+                    <li>
+                        <p>React</p>
+                        <div className="border">
+                            <div className="fill newbie"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>Javascript</p>
+                        <div className="border">
+                            <div className="fill medium"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>CSS</p>
+                        <div className="border">
+                            <div className="fill high"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>Vue</p>
+                        <div className="border">
+                            <div className="fill newbie"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>Redux</p>
+                        <div className="border">
+                            <div className="fill medium"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>React Native</p>
+                        <div className="border">
+                            <div className="fill high"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Skills
